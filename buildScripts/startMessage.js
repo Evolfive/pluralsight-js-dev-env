4df4034bd13c10b2959afebee9fd7e335f102bad
Michel <xexel.mq@gmail.com>
2017-04-08 22:41:26 -0300

    modulo linting finalizado.

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 
 import chalk from 'chalk';
 
-console.log(chalk.green('Iniando app em modo dev...'));
+console.log(chalk.green('Iniando app em modo dev...')); // eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`Eu pagaria ${courseValue} por este incrivel curso!!`);
+console.log(`Eu pagaria ${courseValue} por este incrivel curso!!`); // eslint-disable-line no-console
