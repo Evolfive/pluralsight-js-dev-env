f5382959f5c1f93ec4d6d0cb8f888b977aea7d6d
Michel <xexel.mq@gmail.com>
2017-04-10 00:36:16 -0300

    modulo http call finalizado.

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Nosso primeiro teste', () => {
 });
 
 describe('index.html', () => {
-	it('deve dizer hello', (done) => {
+	it('deve ter h1 que diz Users', (done) => {
 		const index = fs.readFileSync('./src/index.html', 'utf-8');
 		jsdom.env(index, (err, window) => {
 			const h1 = window.document.getElementsByTagName('h1')[0];
-			expect(h1.innerHTML).to.equal("Hello World!!");
+			expect(h1.innerHTML).to.equal("Users");
 			done();
 			window.close();
 		});
